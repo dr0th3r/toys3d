4c2fba136a1574478bac7594cf21f91ed8a3fb83
dr0th3r <dr0th3r.code@gmail.com>
2024-05-26 07:56:41 +0200

    refactor and fix camera

--- main.js
+++ main.js
@@ -1,180 +1,73 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
-import { GlitchPass } from "three/addons/postprocessing/GlitchPass.js";
-import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
-import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass";
 
-//control buttons
-const goBackBtn = document.querySelector(".go-back");
+async function loadRoom() {
+  const loader = new GLTFLoader();
+  const gltf = await loader.loadAsync("assets/room.glb");
+  return gltf;
+}
 
-//screens
-const videoScreen = document.querySelector(".video-screen");
-
-const loader = new GLTFLoader();
-let camera;
-
-const scene = new THREE.Scene();
-
+//renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight, true);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 document.body.appendChild(renderer.domElement);
 
-let mixer;
+//model
+const model = await loadRoom();
+const scene = model.scene;
+const camera = model.cameras[0];
 
-let composer;
-let outlinePass;
-
-const registeredTransitions = {};
-
-function setupTransition(action, goBackBtn, startObjName, screen) {
-  let isOnScreen = false;
-
-  action.loop = THREE.LoopPingPong;
-  action.paused = true;
-  action.play();
-
-  const mixer = action.getMixer();
-  mixer.addEventListener("loop", () => {
-    action.paused = true;
-    if (!isOnScreen) {
-      isOnScreen = true;
-      screen.classList.add("visible");
-    } else {
-      isOnScreen = false;
-    }
-  });
-
-  goBackBtn.addEventListener("click", () => {
-    action.paused = false;
-    screen.classList.remove("visible");
-  });
-
-  registeredTransitions[startObjName] = () => {
-    action.paused = false;
-  };
-}
-
-function getIntersects(e) {
-  const raycaster = new THREE.Raycaster();
-  const mouse = new THREE.Vector2();
-
-  mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-
-  return raycaster.intersectObjects(scene.children, true);
-}
-
-document.addEventListener("click", onClick);
-
-function onClick(e) {
-  const intersects = getIntersects(e);
-
-  if (intersects.length > 0) {
-    const objName = intersects[0].object.name;
-    console.log(objName);
-    if (registeredTransitions[objName]) {
-      registeredTransitions[objName]();
-    }
-  }
-}
-
-function setupOutline() {
-  composer = new EffectComposer(renderer);
-  const renderPass = new RenderPass(scene, camera);
-  composer.addPass(renderPass);
-
-  outlinePass = getOutlineEffect(window, scene, camera);
-  configureOutlineEffectSettings_Default(outlinePass);
-  outlinePass.renderToScreen = true;
-
-  composer.addPass(outlinePass);
-
-  return composer;
-}
-
-function getOutlineEffect(window, scene, camera) {
-  return new OutlinePass(
-    new THREE.Vector2(window.innerWidth, window.innerHeight),
-    scene,
-    camera
-  );
-}
-
-function configureOutlineEffectSettings_Default(outlinePass) {
-  outlinePass.edgeStrength = 1000;
-  outlinePass.edgeGlow = 1;
-  outlinePass.edgeThickness = 2;
-  outlinePass.pulsePeriod = 0;
-  outlinePass.visibleEdgeColor.set("#ffffff");
-  outlinePass.hiddenEdgeColor.set("#190a05");
-}
-
-document.addEventListener("mousemove", onMouseMove, false);
-
-function onMouseMove(e) {
-  const intersects = getIntersects(e);
-
-  addOutline(intersects);
-}
-
-function addOutline(intersects) {
-  outlinePass.selectedObjects = [];
-
-  if (intersects.length > 0) {
-    let objName = intersects[0].object.name;
-
-    if (objName.includes("Cube")) {
-      outlinePass.selectedObjects = [intersects[0].object];
-    }
-  }
-}
-
-loader.load(
-  "assets/room.glb",
-  function (gltf) {
-    scene.add(gltf.scene);
-
-    mixer = new THREE.AnimationMixer(gltf.scene);
-    const clip = THREE.AnimationClip.findByName(
-      gltf.animations,
-      "CameraAction.001"
-    );
-
-    const action = mixer.clipAction(clip);
-    setupTransition(action, goBackBtn, "Cube011", videoScreen);
-
-    console.log(gltf);
-    camera = gltf.cameras[0];
-
-    setupOutline();
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  (error) => {
-    console.error(error);
-  }
+//animations
+const mixer = new THREE.AnimationMixer(model.scene);
+const clip = THREE.AnimationClip.findByName(
+  model.animations,
+  "CameraAction.001"
 );
+const cameraAction = mixer.clipAction(clip);
+cameraAction.play();
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
-directionalLight.position.set(5, 10, 5);
-directionalLight.target.position.set(0, 0, 0);
-scene.add(directionalLight.target);
-scene.add(directionalLight);
+//resize camera
+const zoomOut = 15; //the bigger the number, the more zoomed out the camera will be
+const factor = 2; //the bigger the number, the more zoomed in the camera will be
 
+function resizeCamera() {
+  const aspect = window.innerWidth / window.innerHeight;
+  let width, height;
+
+  if (window.innerWidth > window.innerHeight) {
+    width = zoomOut / factor;
+    height = zoomOut / aspect / factor;
+  } else {
+    width = (zoomOut * aspect) / factor;
+    height = zoomOut / factor;
+  }
+
+  camera.left = -width;
+  camera.right = width;
+  camera.top = height;
+  camera.bottom = -height;
+  camera.updateProjectionMatrix();
+}
+resizeCamera();
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight, true);
+  resizeCamera();
+});
+
+//light
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+//animation loop
 const clock = new THREE.Clock();
 
 function animate() {
   requestAnimationFrame(animate);
 
-  if (mixer) mixer.update(clock.getDelta());
-
-  if (composer) composer.render();
+  mixer.update(clock.getDelta());
 
   renderer.render(scene, camera);
 }
+
 animate();
